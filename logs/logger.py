228700fbf3cfab20d5ba4c9e0e48e8bdf44381c4
Sergey Kazantsev <ksa@MacBook-Pro-Sergey.local>
2026-06-05 14:48:10 +0300

    added logger + new folder in gitignore

---
+++ logs/logger.py
@@ -0,0 +1,20 @@
+import json
+import logging
+from pathlib import Path
+
+
+def logger():
+    folder = Path(__file__).parent
+    cfg = json.loads((folder / 'config.json').read_text(encoding="utf-8"))
+
+    if not cfg.get('logging_enabled', True):
+        return logging.getLogger('logs')
+
+    log_file = folder / cfg.get('log_file')
+
+    logging.basicConfig(level=cfg.get('log_level', 'INFO').upper(), 
+                        format='%(asctime)s - %(filename)s - %(levelname)s - %(message)s',
+                        handlers = [logging.FileHandler(log_file, encoding='utf-8'), logging.StreamHandler()], force=True)
+    return logging.getLogger('logs')
+
+
